3211a75a42f99ae0b10b69506ce75ab3519fb488
Marcelo Archanjo <marcelo_archanjo@yahoo.com.br>
2021-06-22 11:56:45 -0300

    Script to BERTimbau Large training

---
+++ mrat-sql-gap/BERTimbau-large.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+echo "Folders preparation"
+Name="BERTimbau-large"
+
+
+mkdir configs/${Name}
+mkdir logdir/${Name}-pt-train
+mkdir ie_dirs/${Name}-pt-train
+mkdir models/${Name}
+mkdir models/${Name}/pretrained_checkpoint
+
+cd models/${Name}/pretrained_checkpoint
+curl https://huggingface.co/neuralmind/bert-large-portuguese-cased/resolve/main/added_tokens.json -o added_tokens.json
+curl https://huggingface.co/neuralmind/bert-large-portuguese-cased/resolve/main/config.json -o config.json
+curl https://cdn-lfs.huggingface.co/neuralmind/bert-large-portuguese-cased/9af4f60f0bdd71e483baf8a1dd3e3dc509ceeaa7dd2007ed63f110b5c990e6e6 -o flax_model.msgpack
+curl https://cdn-lfs.huggingface.co/neuralmind/bert-large-portuguese-cased/48f211712fdad2263e35c368b0ec79ad635c2df0acb275152e0f7cbd165bb7ca -o pytorch_model.bin
+curl https://huggingface.co/neuralmind/bert-large-portuguese-cased/resolve/main/special_tokens_map.json -o special_tokens_map.json
+curl https://huggingface.co/neuralmind/bert-large-portuguese-cased/resolve/main/tokenizer_config.json -o tokenizer_config.json
+curl https://huggingface.co/neuralmind/bert-large-portuguese-cased/resolve/main/vocab.txt -o vocab.txt
+cd ..
+cd ..
+cd ..
+
+
+echo "Symbolic link to the pretrained checkpoint"
+rm -r pretrained_checkpoint
+ln -s models/${Name}/pretrained_checkpoint pretrained_checkpoint
+
+
