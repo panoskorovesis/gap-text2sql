3cd604fe2d57bd92401a1cff64dadbffb7b3e976
Marcelo Archanjo <marcelo_archanjo@yahoo.com.br>
2021-09-02 13:13:57 -0300

    Update BART_large.sh
    Include the original checkpoint

--- mrat-sql-gap/BART_large.sh
+++ mrat-sql-gap/BART_large.sh
@@ -20,6 +20,7 @@ cd logdir/${Name}-en-train
 mkdir bs=12,lr=1.0e-04,bert_lr=1.0e-05,end_lr=0e0,att=1
 cd bs=12,lr=1.0e-04,bert_lr=1.0e-05,end_lr=0e0,att=1
 gdown --id 1F4R-WkJKtJ4lFni3q4lBug6tzSo0H5Qe
+curl https://gap-text2sql-public.s3.amazonaws.com/checkpoint-artifacts/gap-finetuned-checkpoint -o model_checkpoint-00041000
 cd ..
 cd ..
 cd ..
